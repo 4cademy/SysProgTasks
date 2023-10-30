ec9f23a3ad8071567373fc532a9af2dc7a79c384
Marcel Beyer <marcelbeyer412@gmx.de>
2023-10-30 07:59:31 +0100

    Added Makefile and myshell.cpp

---
+++ task3/myshell.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <filesystem>
+
+#include "readline/readline.h"
+#include "readline/history.h"
+
+int main ()
+{
+    char *line;
+    while (true) {
+        line = readline (std::filesystem::current_path().c_str());
+        if (!line) break;
+        printf ("You entered: %s\n", line);
+        free (line);
+    }
+    return 0;
+}
