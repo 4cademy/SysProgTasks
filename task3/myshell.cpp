166c9a1e43970d866dcb4004c072f2d400a3ef08
Marcel Beyer <marcelbeyer412@gmx.de>
2023-11-16 16:18:14 +0100

    Implemented functions to store commands

--- task3/myshell.cpp
+++ task3/myshell.cpp
@@ -5,11 +5,21 @@
 #include <cstdio>
 #include <cstdlib>
 #include <filesystem>
+#include <vector>
 #include "readline/readline.h"
 #include "readline/history.h"
 #include "parse.tab.h"
 
 extern int parse_string(const char* in); // Declare the function from parse.y
+
+struct {
+    std::string name;
+    std::vector<char*> arguments;
+    std::string input;
+    std::string output;
+} typedef command;
+
+std::vector<command> command_list;
 
 int main ()
 {
@@ -24,6 +34,26 @@ int main ()
     return 0;
 }
 
-void Execute(){
+void new_command(char* name){
+    command_list.push_back(command());
+    command_list.back().name = name;
+    command_list.back().arguments = std::vector<char*>();
+    command_list.back().input = "";
+    command_list.back().output = "";
+}
+
+void add_argument(char* arg){
+    command_list.back().arguments.push_back(arg);
+}
+
+void set_input(char* input){
+    command_list.back().input = input;
+}
+
+void set_output(char* output){
+    command_list.back().output = output;
+}
+
+void execute(){
     printf("Executing...\n");
 }
