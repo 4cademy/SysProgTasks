f5b86f16ae633e8e14aec1715e1ebfbffde59742
Marcel Beyer <marcelbeyer412@gmx.de>
2023-11-17 12:27:20 +0100

    Started implementing execution with fork(). Wrong arguments are passed at the moment

--- task3/myshell.cpp
+++ task3/myshell.cpp
@@ -6,17 +6,19 @@
 #include <cstdlib>
 #include <filesystem>
 #include <vector>
+#include <fcntl.h>
 #include "readline/readline.h"
 #include "readline/history.h"
 #include "parse.tab.h"
 #include <iostream>
+#include <csignal>
+#include <sys/wait.h>
 
 #define Log(x) std::cout << x << std::endl;
 
 extern int parse_string(const char* in); // Declare the function from parse.y
 
 struct {
-    std::string name;
     std::vector<char*> arguments;
     std::string input;
     std::string output;
@@ -30,8 +32,7 @@ int main ()
     while (true) {
         line = readline (std::filesystem::current_path().append("$ ").c_str());
         if (!line) break;
-        printf ("Parsing: %s\n", line);
-        parse_string(line); // Call the function with the input line
+        parse_string(line); // Call the lexer with the input line
         free (line);
     }
     return 0;
@@ -39,8 +40,8 @@ int main ()
 
 void new_command(char* name){
     command_list.push_back(command());
-    command_list.back().name = name;
     command_list.back().arguments = std::vector<char*>();
+    command_list.back().arguments.push_back(name);
     command_list.back().input = "";
     command_list.back().output = "";
 }
@@ -59,7 +60,6 @@ void set_output(char* output){
 
 void print_commands(){
     for (auto & command : command_list) {
-        std::cout << command.name << " ";
         for (auto & argument : command.arguments) {
             std::cout << argument << " ";
         }
@@ -70,5 +70,28 @@ void print_commands(){
 
 void execute(bool background){
     print_commands();
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        if(!command_list[0].input.empty()){
+            // Redirect stdin to file
+            close(STDIN_FILENO);
+            int fd = open(command_list[0].input.c_str(), O_RDONLY);
+            if (STDIN_FILENO != fd) {
+                dup2(fd, STDIN_FILENO);
+            }
+        }
+        std::cout << "Here: " << std::endl;
+        std::cout << command_list[0].arguments[0] << std::endl;
+        std::cout << command_list[0].arguments.data() << std::endl;
+
+        if(execvp(command_list[0].arguments[0], command_list[0].arguments.data()) == -1){
+            std::cout << "Error executing" << std::endl;
+        }
+        exit(0);
+    } else if (pid > 0){
+        wait(nullptr);
+    }
+
     command_list.clear();
 }
